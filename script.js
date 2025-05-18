b8e49dc59ed6295910c99948eddf75cfc849e657
Muhammer Elek <muhammerelek@users.noreply.github.com>
2025-05-18 13:58:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ function generateQR() {
   const phone = document.getElementById('phone').value.trim();
   const data = `tel:${phone}`;
   const canvas = document.getElementById('qr-code');
-  QRCode.toCanvas(canvas, data, function (error) {
+  QRCode.toCanvas(canvas, data, {
+    width: 256, // varsayılan 256 px (A4 için ideal)
+    margin: 2
+  }, function (error) {
     if (error) console.error(error);
     else console.log('QR kodu oluşturuldu!');
   });
+}
+
+function printQR() {
+  window.print();
 }
